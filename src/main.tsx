3546c6645571b3e4053a6c1c56f43834caa06081
David Ogungbemi <daveedog2003@gmail.com>
2025-09-30 16:43:51 +0100

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,13 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./stores/store";
+import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <Analytics />
     </Provider>
   </React.StrictMode>
 );
