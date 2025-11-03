644503fb32b719540cfe69c8fd56908f6262ac1e
David Ogungbemi <daveedog2003@gmail.com>
2025-11-03 20:30:38 +0100

    update

--- src/pages/account/accountType.tsx
+++ src/pages/account/accountType.tsx
@@ -69,7 +69,7 @@ export default function AccountType() {
         )}
 
         {/* Account Type Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-10">
           {/* Buyer/Customer Option */}
           <button
             onClick={() => setAccountType("buyer")}
